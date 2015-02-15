c50c4cda70954895d75503853dbea93eba25b2c0
Ekechi Ikenna <luccy559slim@gmail.com>
2015-02-15 16:43:03 +0100

    setup s3 and styled devise views

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -2,16 +2,15 @@ class Hospital < ActiveRecord::Base
   has_many :hospital_likes
   has_many :users, through: :hospital_likes, class_name: 'User'
 
-  has_attached_file :image, :styles => { :large => "650x700>",
-                                         :medium => "300x300>",
-                                         :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { large: "650x700>",
+                                         medium: "300x300>",
+                                         thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /^image\/(png|jpeg)/,
+                                    message: 'only (png/jpeg) images'
 
-  validates_attachment :image, :presence => true,
-                       :content_type => { :content_type => "image/jpeg" },
-                       :size => { :in => 0..10.megabytes }
-
+  validates_attachment :image, presence: true,
+                       size: { in: 0..10.megabytes }
 
   HOSPITAL_TYPE = ["General", "Specialist", "Psychiatric",
                    "Fertility", "Eye Clinic", "Dental Clinic" ,
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :image, styles: { large: "650x700>",
+                                      medium: "300x300>",
+                                      thumb: "100x100>" },
+                    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /^image\/(png|jpeg)/,
+                                    message: 'only (png/jpeg) images'
+
+  validates_attachment :image, presence: true,
+                       size: { in: 0..5.megabytes }
+
 end
