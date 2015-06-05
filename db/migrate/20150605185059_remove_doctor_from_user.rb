c5080e622b5290962f602ee33fa6f90bbf01c80a
Adefila Sunday <sunday.adefila@andela.co>
2015-06-05 22:49:01 +0100

    default test users added

--- db/migrate/20150605185059_remove_doctor_from_user.rb
+++ db/migrate/20150605185059_remove_doctor_from_user.rb
@@ -1,6 +1,6 @@
 class RemoveDoctorFromUser < ActiveRecord::Migration
   def change
     remove_column :users, :doctor, :boolean
-    change_column_default(:users, :role, 'user')
+    change_column_default(:users, :role, "")
   end
 end
