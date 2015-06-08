a2fc8b8510478fbde69af329a3ccdb59ef743a18
Ekechi Ikenna <luccy559slim@gmail.com>
2015-06-08 23:34:55 +0100

    unverify

--- app/models/doctor_list.rb
+++ app/models/doctor_list.rb
@@ -1,4 +1,13 @@
 class DoctorList < ActiveRecord::Base
   has_many :users
   has_many :hospitals
+
+  after_commit :remove_doctor, on: :destroy
+
+  protected
+
+  def remove_doctor
+    user.verified = false
+    user.save
+  end
 end
--- app/models/hospital_list.rb
+++ app/models/hospital_list.rb
@@ -1,4 +1,13 @@
 class HospitalList < ActiveRecord::Base
   has_many :users
   has_many :hospitals
+
+  after_commit :remove_hospital, on: :destroy
+
+  protected
+
+  def remove_hospital
+    hospital.verified = false
+    hospital.save
+  end
 end
