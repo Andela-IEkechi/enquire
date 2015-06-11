e081ddd4fa13da0ad49f8ee23b0dee8f395e7b3d
Ekechi Ikenna <luccy559slim@gmail.com>
2015-06-11 08:15:12 +0100

    profile page changes

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,10 @@
 module ProfilesHelper
+
+  def seen_last time
+    if time < Date.today
+      time.strftime("Last seen on  %A, %d %b %Y %l:%M %p")
+    else
+      time.strftime("Last seen at  %I:%M%p")
+    end
+  end
 end
