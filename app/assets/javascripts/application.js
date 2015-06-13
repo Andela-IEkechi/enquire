bf82bc98d1316b005eb9821004e140c54e79ab82
Adefila Sunday <sunday.adefila@andela.co>
2015-06-13 23:34:46 +0100

    cickable area on user profile leading to dashboard enlarged

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,9 @@
 // THE REQUIRE TREE LINE TO MAKE THE SHIT WORK
 // SO FUCKED!!!
 //
-//= require jquery
 //= require jsapi
 //= require chartkick
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require_tree .
+//= require jquery
