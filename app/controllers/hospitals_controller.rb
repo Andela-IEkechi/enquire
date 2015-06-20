4a294ed8f1f53dc9a514f886a61b99530d2f0730
Adefila Sunday <sunday.adefila@andela.co>
2015-06-20 15:20:31 +0100

    styling home page

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -78,6 +78,6 @@ class HospitalsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def hospital_params
-      params.require(:hospital).permit(:user_id, :name, :description, :image, :verified, :address, :classification)
+      params.require(:hospital).permit(:user_id, :name, :description, :image, :verified, :address, :classification, :phone_number)
     end
 end
