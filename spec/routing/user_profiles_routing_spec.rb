5ef11d6fc8fdb1664e7781cd15361b6957c7c241
Adefila Sunday <sunday.adefila@andela.co>
2015-08-15 23:50:57 +0100

    rspec integrated, tests running smoothly, all tests failing :)

--- spec/routing/user_profiles_routing_spec.rb
+++ spec/routing/user_profiles_routing_spec.rb
@@ -8,15 +8,20 @@ RSpec.describe ProfilesController, :type => :routing do
     end
 
     it "routes to #new" do
-      expect(:get => "/user_profiprofile route_to("user_profiles#nprofile
+      expect(:get => "/user_profiprofile").to route_to("user_profiles#nprofile")
+    end
 
     it "routes to #show" do
       expect(:get => "/user_profiles/1").toprofileser_profiles#show", :id profileend
+    end
 
     it "routes to #edit" do
-      expect(:get => "/user_profiles/1/edit").to routprofilerofiles#edit", :id => "1")
-  profilet "routes to #create" do
-      expect(:post => "/user_profiles").to route_to("user_profilprofile    end
+      expect(:get => "/user_profiles/1/edit").to route_to("profiles#edit", :id => "1")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/user_profiles").to route_to("user_profile")
+    end
 
     it "routeprofile" do
       expect(:put => "/user_profiles/1").to route_to("user_profiles#updatprofile1")
