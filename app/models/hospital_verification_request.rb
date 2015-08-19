80cef717aebb26db61d2d058cd23fd82d8d84d95
Adefila Sunday <sunday.adefila@andela.co>
2015-08-19 12:19:14 +0100

    the hospital_verification_request model successfully covered with test suits

--- app/models/hospital_verification_request.rb
+++ app/models/hospital_verification_request.rb
@@ -1,20 +1,24 @@
 class HospitalVerificationRequest < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user #todo :manager, -> { where(role: "manager") }, class_name: "User", foreign_key: 'user_id'
   belongs_to :hospital
 
   validates :message, :user, :hospital, presence: true
-  validates :hospital_id, uniqueness: { message: "already has a verification request open!" }
-  validate :eligible?
+  validates :hospital, uniqueness: { message: "already has a verification request open!" }
+  validate :hospital_eligible?# :user_eligible?
 
-  after_commit :update_hospital, on: :destroy
+  after_destroy :verify_hospital
 
   protected
 
-    def eligible?
-      errors[:base] << "Already verified" unless self.hospital.verified == false
+    # def user_eligible?
+    #   errors[:base] << "must be a manager" unless self.user.role == 'manager'
+    # end
+
+    def hospital_eligible?
+      errors.add(:hospital, "is already verified") unless self.hospital and self.hospital.verified == false
     end
 
-    def update_hospital
+    def verify_hospital
       ActiveRecord::Base.transaction do
         self.hospital.update_attributes(verified: true)
         HospitalList.create!(user_id: user.id, hospital_id: hospital.id)
