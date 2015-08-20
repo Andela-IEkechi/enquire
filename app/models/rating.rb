e7057197cbe3500aaadcfb185d3612d470b706b9
Adefila Sunday <sunday.adefila@andela.co>
2015-08-20 12:33:01 +0100

    the ratings  model successfully covered with test suits

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,29 @@
 class Rating < ActiveRecord::Base
   belongs_to :article
-  belongs_to :user
+  belongs_to :user #todo what type of user can rate an article? client or doctor?
+
+  before_save :update_rating_if_already_exists, if: :new_record?
+
+  validates :article, :user, :score, presence: true
+  # validates :user, uniqueness: { scope: :article, message: "have already rated this article" }#todo users should be able to override their previous ratings
+
+  def update_rating_if_already_exists
+    original_rating = Rating.where(user: self.user, article: self.article)
+    if original_rating.blank?
+      true
+    else
+      original_rating[0].update_attribute(:score, self.score)
+      false
+    end
+  end
+
+  # todo how could we make this more scalable?
+  # def self.rate
+  #   original_rating = Rating.where(user: self.user, article: self.article)
+  #   if original_rating.blank?
+  #     self.save
+  #   else
+  #     original_rating[0].update_attribute(:score, self.score)
+  #   end
+  # end
 end
