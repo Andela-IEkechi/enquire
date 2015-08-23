6ebc1f9f28cef205d2d2ca61df2913528aaac589
Adefila Sunday <sunday.adefila@andela.co>
2015-08-23 19:21:53 +0100

    the profiles controller totally covered with test suites

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :hospitals
   resources :answers, except: :show
-  resources :profiles, only: [:show, :index]
+  resources :profiles, only: [:show, :index] #todo do we really need show????
   resource  :dashboard, only: :show
   resources :doctors
   resources :hospital_likes, only: [:new, :create, :destroy]
